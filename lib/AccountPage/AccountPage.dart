f567cdf3458da749b035f336e5245ca2fa31cd02
TJWRii <s1912558@ed.ac.uk>
2024-03-10 17:47:35 +0000

    add firebase config

--- lib/AccountPage/AccountPage.dart
+++ lib/AccountPage/AccountPage.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() => runApp(MyApp());
+// void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -16,6 +23,34 @@ class AccountPage extends StatelessWidget {
   // TextEditingControllers to collect username and password
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+
+  Future<void> _login() async {
+    final String email = _usernameController.text.trim();
+    final String password = _passwordController.text.trim();
+
+    if (email.isEmpty || password.isEmpty) {
+      // Show error message if email or password is empty
+      print('Email and password cannot be empty');
+      return;
+    }
+
+    try {
+      // 使用Firebase Auth进行邮箱和密码登录
+      final UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      // 登录成功
+      print('Login successful: ${userCredential.user}');
+
+      // 可以在这里进行页面跳转或状态更新
+    } on FirebaseAuthException catch (e) {
+      // 处理登录过程中可能发生的错误
+      print('Login error: ${e.message}');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,7 +71,8 @@ class AccountPage extends StatelessWidget {
                 labelText: 'Username',
                 border: OutlineInputBorder(),
               ),
-              keyboardType: TextInputType.emailAddress, // Use email input type for usernames
+              keyboardType: TextInputType
+                  .emailAddress, // Use email input type for usernames
             ),
             SizedBox(height: 20), // Space between input fields
             // Password TextField
@@ -53,11 +89,7 @@ class AccountPage extends StatelessWidget {
             // Login Button
             ElevatedButton(
               child: Text('Login'),
-              onPressed: () {
-                // Implement login logic
-                print('Username: ${_usernameController.text}');
-                print('Password: ${_passwordController.text}');
-              },
+              onPressed: _login, // Update this line
             ),
           ],
         ),
