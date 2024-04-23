c8edf92602d800e72163a28f1fbab5cad3ef65ac
TJWRii <s1912558@ed.ac.uk>
2024-04-23 10:55:24 +0100

    fix running on ios simulator

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,12 +3,11 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  file_selector_windows
+  desktop_webview_auth
   firebase_auth
   firebase_core
   geolocator_windows
   permission_handler_windows
-  url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
